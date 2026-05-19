da0c0b5c6a98b49000b45cbca12db34bca7de71c
Aniqamir03 <amiraniq008@gmail.com>
2026-05-19 23:21:42 +0800

    Update dashboard.py

--- views/dashboard.py
+++ views/dashboard.py
@@ -932,7 +932,7 @@ with col_kanan:
         </script>
         """
 
-        components.html(html_kod, height=100)
+        components.html(html_kod, height=430)
     else:
         st.info("Ruangan memori kosong.")
 
