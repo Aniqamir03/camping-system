6ea4f362ee91d0119adca91410483d12419fa500
Aniqamir03 <amiraniq008@gmail.com>
2026-05-19 23:28:21 +0800

    Update dashboard.py

--- views/dashboard.py
+++ views/dashboard.py
@@ -865,7 +865,7 @@ with col_kanan:
 
             @media (max-width: 520px) {{
     .memory-slider {{
-        height: 160px; /* UBAH TINGGI GAMBAR MOBILE DI SINI */
+        height: 220px; /* UBAH TINGGI GAMBAR MOBILE DI SINI */
     }}
         </style>
 
